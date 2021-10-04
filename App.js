03fe515b98befde791cbca73cf55148afc9509a2
oshini-98 <it19990446@my.sliit.lk>
2021-10-05 00:56:34 +0530

    Initial Push

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+//import Login from './src/components/login';
+import Login from './src/screens/login';
+import Profile from './src/screens/profile';
+import PjComletion from './src/screens/pjCompletion';
+import JReceipt from './src/screens/jReceipt';
+import RSmartCard from './src/screens/rSmartCard';
+
+const App = (props) => {
+  return (
+  // <Login/>
+  // <Profile/>
+  // <PjComletion/>
+  <JReceipt/>
+  // <RSmartCard/>
+  )
+}
+export default App;
